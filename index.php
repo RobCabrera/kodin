04869d51468688036c84c2bf1df3a22df7e166b6
robcabrera <rob@robertocabrera.us>
2014-12-05 20:02:15 +0000

    One change

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 <?php
 
 
-//Add this to every single page you create. This is DATADOG.
-require("datadog-tracker.php");
-
-
 echo "a cool app is about to be created. Stay tunned for more info...";
 
 
