916812c75b450822d362d77b2a7429f47a8c15a0
Roberto Cabrera <robertocabrera15@hotmail.com>
2014-12-05 14:56:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,39 +9,4 @@ require("datadog-tracker.php");
 echo "a cool app is about to be created. Stay tunned for more info...";
 
 
-//Connect to a database using pdo
-/*
-include('con-db.php');
-
-try{
-	$db = new PDO('mysql:host='.$DBHost.';dbname='.$DBName.';charset=utf8',$DBUsername,$DBPassword,array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
-	$stmt = $db->prepare("SELECT * FROM Examples");
-
-	$stmt->execute();
-
-	$row = $stmt->rowCount();
-   //;
-   
-   echo $row . " record(s) where found in the database. <br><br>";
-   
-   if ($row > 0 ){
-
-        for ($i = 1; $i <= $row; $i++) {
-                     
-            $result = $stmt->fetch();     
-            echo "Record ".$i."<br>";
-            echo date('D m/d/Y h:i:sa T',strtotime($result['DateInserted']))."<br>";
-            echo $result['Text']. "<br><br>";
-
-        }
-   }
-
-}catch(PDOException $e) {
-	//echo 'ERROR: ' . $e->getMessage();
-	echo $e->getMessage();
-}
-
-*/
-
 ?>
