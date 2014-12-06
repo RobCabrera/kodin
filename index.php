96b85624010515519c6f681851dc611912549dd0
koding user <zameermfm@gmail.com>
2014-12-06 03:35:29 +0000

    I added few lines of code

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 
 
-echo "a cool app is about to be created. Stay tunned for more info...";
-
+echo "a cool app is about to be created. Stay tunned for more info...<br> ";
+echo "i added few things";
 
 ?>
