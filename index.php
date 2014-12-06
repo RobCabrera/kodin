090d0230b2121b1c8a42e7e4939739f197a8010c
koding user <zameermfm@gmail.com>
2014-12-06 10:47:57 +0000

    everything except slideshow is completed

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
+<?php
+session_start();
 
+$_SESSION["color"] = 'red';
+
+ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> <html xmlns="http://www.w3.org/1999/xhtml"> <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css' rel='stylesheet' /> 
 <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
