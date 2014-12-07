8e421a4db7252cd804281f8bd0175a973ba0c55e
koding user <zameermfm@gmail.com>
2014-12-07 04:03:39 +0000

    downnload and email added, change the credentials

--- show.php
+++ show.php
@@ -1,61 +1,23 @@
 <?php 
 
+ob_start();
 
-
-
-if( $_POST["submit"]=="Download"){
+  if( $_POST["submit"]=="Download"){
     
     $no = mt_rand(100,10000);
     $filename = $no.'document.html';
+     header("Cache-Control: public");
+     header("Content-Description: File Transfer");
+     header("Content-Disposition: attachment; filename=$filename");
+     header("Content-Type: application/octet-stream; ");
+     header("Content-Transfer-Encoding: binary");
     
+    $impress = file_get_contents('./js/impress.js', true);
+    $css = file_get_contents('./css/impress-demo.css', true);
+        
     
-    header("Cache-Control: public");
-    header("Content-Description: File Transfer");
-    header("Content-Disposition: attachment; filename=$filename");
-    header("Content-Type: application/octet-stream; ");
-    header("Content-Transfer-Encoding: binary");
-    
-}elseif($_POST["submit"]=="Email"){
-    
-    //Using PHPMailer 
-    require 'PHPMailer/PHPMailerAutoload.php';
-
-    $mail = new PHPMailer;
-    
-    $mail->isSMTP();
-    $mail->Host = 'ssrs.reachmail.net'; 
-    $mail->SMTPAuth = plain;                            
-    $mail->Username = 'ROBERTOC1\\admin';              
-    $mail->Password = '!@1VbD@#';                       
-    $mail->SMTPSecure = 'tls';                        
-    $mail->Port = 587;    
-    
-    //This is a free smtp, therefore, it will take some time to deliver the message.. it sucks! I know... Feel free to plug in your SMTP info.
-    
-    $email = $_POST['email'];
-    $text = $_POST["fulltext"];
-   
-    $mail->From = 'rob@robertocabrera.us';
-    $mail->FromName = 'system';
-    $mail->addAddress($email);     // Add a recipient
-    
-    
-   // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-    $mail->isHTML(true);                                  // Set email format to HTML
-    
-    $mail->Subject = 'Test - using free smtp ';
-    $mail->Body    = 'Attached, please find your zip. files' . $text;
-   // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-    
-    if(!$mail->send()) {
-        echo 'Message could not be sent.<br>';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-    } else {
-        echo 'Message has been sent';
-    }
-    
-}else{
+ }
+ 
 ?>
 <html lang="en">
 <head>
@@ -68,7 +30,6 @@ if( $_POST["submit"]=="Download"){
 session_start();
 $_SESSION["color"] = 'green';
 ?>
-
 
 
 
@@ -90,10 +51,14 @@ $realsections = explode('_', $sections);
 <!--
 For example this fallback message is only visible when there is `impress-not-supported` class on body.
 -->
+
+<?php if(!$_POST["submit"]=="Email"){ ?>
 <div class="fallback-message" style="visiblity:hidden">
 <p>Your browser <b>doesn't support the features required</b> 
 </div>
-
+<?php
+} 
+?>
 <div id="impress">
 
 
@@ -123,24 +88,86 @@ $y = -1500;
 <div id="overview" class="step" data-x="3000" data-y="1500" data-scale="10">
 </div>
 </div>
-
+<?php if(!$_POST["submit"]=="Email"){ ?>
 <div class="hint">
 <p>Use a spacebar or arrow keys to navigate</p>
 </div>
+<?php } ?>
 <script>
 if ("ontouchstart" in document.documentElement) {
 document.querySelector(".hint").innerHTML = "<p>Tap on the left or right to navigate</p>";
 }
 </script>
 
-<script src="js/impress.js"></script>
+
+<?php
+
+  if(isset($impress)){
+      echo "<script>".$impress."</script>";
+  }else{
+      echo "<script src='js/impress.js'></script>";
+  }
+?>
 <script>impress().init();</script>
 <?php 
-    } 
+
+
+
+
+if($_POST["submit"]=="Email"){
+     if(isset($css)){
+      echo "<style>".$css."</style>";
+  }
+    //Using PHPMailer 
+     require 'PHPMailer/PHPMailerAutoload.php';
+
+     $mail = new PHPMailer;
+    
+    $mail->isSMTP();
+     $mail->Host = 'smtp.gmail.com'; 
+    $mail->SMTPAuth = plain;                            
+    $mail->Username = 'your email';              
+    $mail->Password = 'that app password';                       
+    $mail->SMTPSecure = 'tls';                        
+    $mail->Port = 587;    
+    
+    //This is a free smtp, therefore, it will take some time to deliver the message.. it sucks! I know... Feel free to plug in your SMTP info.
+    
+    $email = $_POST['email'];
+    $text = $_POST["fulltext"];
+   
+    $mail->From = 'system@agreed.com';
+    $mail->FromName = 'system';
+    $mail->addAddress($email);     // Add a recipient
+
+
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $html = ob_get_contents();
+    ob_end_clean();
+    
+    $css = file_get_contents('./css/impress-demo.css', true);
+    $mail->Subject = 'Your document has arrived.';
+    $mail->Body = $html;
+  // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+    if(!$mail->send()) {
+        echo 'Message could not be sent.<br>';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message has been sent';
+    }
+    
+  }
+
 
 
 
 ?>
 
+<?php
+
+ 
+?>
 </body>
 </html>
