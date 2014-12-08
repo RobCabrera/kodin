4d02510ff2155f725f1ed251174c565ce4d7b6a3
Roberto Cabrera <robertocabrera15@hotmail.com>
2014-12-07 22:47:30 -0500

    Update show.php

--- show.php
+++ show.php
@@ -1,83 +1,88 @@
 <?php 
-
+session_start();
 ob_start();
-  if( $_POST["submit"]=="Download"){
-  
-  
-    $no = mt_rand(100,10000);
-    $filename = $no.'document.html';
-     header("Cache-Control: public");
-     header("Content-Description: File Transfer");
-     header("Content-Disposition: attachment; filename=$filename");
-     header("Content-Type: application/octet-stream; ");
-     header("Content-Transfer-Encoding: binary");
-    
-    $impress = file_get_contents('./js/impress.js', true);
-    $css = file_get_contents('./css/impress-demo.css', true);
-            if(isset($css)){
-      echo "<style>".$css."</style>";
-  }
-    
- }
- 
+
+function base_url($atRoot=FALSE, $atCore=FALSE, $parse=FALSE){
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $http = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
+            $hostname = $_SERVER['HTTP_HOST'];
+            $dir =  str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+
+            $core = preg_split('@/@', str_replace($_SERVER['DOCUMENT_ROOT'], '', realpath(dirname(__FILE__))), NULL, PREG_SPLIT_NO_EMPTY);
+            $core = $core[0];
+
+           $tmplt = $atRoot ? ($atCore ? "%s://%s/%s/" : "%s://%s/") : ($atCore ? "%s://%s/%s/" : "%s://%s%s");
+            $end = $atRoot ? ($atCore ? $core : $hostname) : ($atCore ? $core : $dir);
+            $base_url = sprintf( $tmplt, $http, $hostname, $end );
+        }
+        else $base_url = 'http://localhost/';
+
+        if ($parse) {
+           $base_url = parse_url($base_url);
+            if (isset($base_url['path'])) if ($base_url['path'] == '/') $base_url['path'] = '';
+        }
+        return $base_url;
+    }
+
+ if(isset($_POST["document"])){
+        
+        $string = $_POST["document"]; //getting the text
+        $data  = preg_split("#\n\s*\n#Uis", $string); //spliting to sections
+        //display topic of first section
+        $sects = array();
+        $tops = array();
+        
+        foreach($data as $key1 => $val1) {
+           $firstElement = true;
+          $dataN= preg_split("#\n#Uis", $data[$key1]);
+          foreach($dataN as $key => $val) {
+            if($firstElement) {
+              $firstElement = false;
+            } else {
+              array_push($sects, $val);
+            }
+          }
+        }
+        
+        foreach($data as $key1 => $val1) {
+         $firstElement2 = true;
+          $dataN= preg_split("#\n#Uis", $data[$key1]);
+          foreach($dataN as $key => $val) {
+             if($firstElement2) {
+              array_push($tops, $val);
+              $firstElement2 = false;
+            } else {
+              break;
+            }
+          }
+        }
+        
+        $topics = implode('_', $tops);
+        $sections = implode('_', $sects);
+        
+    }
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=1024" />
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<title>Document presentation</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=1024" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<title>Your Own Interactive TOS</title>
 
-<?php 
-session_start();
-$_SESSION["color"] = 'green';
-?>
+	
 
-<link href="css/impress-demo.css" rel="stylesheet" />
-<link rel="apple-touch-icon" href="apple-touch-icon.png" />
-<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?skin=desert"></script>
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-<link href='css/custom-style.css' rel='stylesheet' /> 
-    <link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css' rel='stylesheet' /> 
-    <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
-<style>
-	.custom-wrap{
-		width: 100%;
-		max-width: 1110px;
-        height: 600px;
-        border: 1px solid #2e6da4;
-        background: #337ab7;
-        overflow: hidden;
-        position: relative;
-        color: #fff;
-        margin: 10px auto;
-	}
-	
-	.custom-header{
-	    background-color: #f8f8f8;
-	    border-color: #e7e7e7;
-	    position: relative;
-        min-height: 50px;
-        margin-bottom: 20px;
-        border: 1px solid transparent;
-        width: 100% !important;
-        z-index: 9999;
-	}
-	
-	.header-wrapper{
-	    width: 980px;
-	    margin: 10px auto;
-	}
-	
-	.header-buttons button{
-	    
-	    
-	}
-	
-	
-</style>
+	<link href="css/impress-demo.css" rel="stylesheet" />
+	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+	<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?skin=desert"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="js/customjs.js"></script>
+	<link href='css/custom-style.css' rel='stylesheet' />
+	<link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css' rel='stylesheet' /> 
+	<script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
+
 
 </head>
 
@@ -85,16 +90,17 @@ $_SESSION["color"] = 'green';
 <body class="impress-not-supported">
 
     <?php 
-    $topics = $_POST["topics"]; //getting the text
-    $sections = $_POST["sections"]; //getting the text
+    //$topics = $_POST["topics"]; //getting the text
+    //$sections = $_POST["sections"]; //getting the text
     $realtopics = explode('_', $topics);
+	$realtopicsN = explode('_', $topics);
     $realsections = explode('_', $sections);
     ?>
     
     <!--
     For example this fallback message is only visible when there is `impress-not-supported` class on body.
     -->
-     <nav class="navbar navbar-default" role="navigation"> 
+     <nav class="navbar navbar-inverse custom-nav" role="navigation"> 
     
         <div class="container">
             <div class="container-fluid">
@@ -111,16 +117,10 @@ $_SESSION["color"] = 'green';
                 
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <button type="button" class="btn btn-warning navbar-btn">
-                            <span class='glyphicon glyphicon-floppy-save' aria-hidden='true'></span> Download
-                        </button>
-                        <button class='btn btn-info btn-large' type='button' name='email' value='Email'>
-					        <span class='glyphicon glyphicon-envelope' aria-hidden='true'></span> Email
-					    </button>
-					    <button class='btn btn-info btn-danger' type='button' name='sourcecode'>
-					        <span class='glyphicon glyphicon-tasks' aria-hidden='true'></span> Source Code
-					    </button>
+                   <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="demo.php">Demo</a></li>
+                        <li><a href="createone.php">Create Your Own </a></li>
                     </ul>
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
@@ -135,34 +135,146 @@ $_SESSION["color"] = 'green';
         <div id="impress">
         
         
-        <?php 
+        <?php
+        
+        
         
         $x = -1000;
-        $y = -1500;
+        $y = -2000;
+        $r = 0;
+        $s = 1;
+        // Pick Random, color from list
+        $array = Array('color:white');
+        $ColoRrand = array_rand($array);
         
+        // Pick Random fonts
+        $Farray = Array('font-family:serif', 'font-family:Comic Sans MS', 'font-family:Gadget', 'font-family:Arial ',
+                       'font-family:sans-serif', 'font-family:Helvetica');
+        $FontRrand = array_rand($Farray);
         
-        for ($i=0;$i<count($realtopics);$i++){
+        // Pick Random text blockmovment
+             $SectionMovearray = Array('show sliderleft','show sliderup', 'show sliderright','show rotateright','show rotateleft');
+             $SectionMove = array_rand($SectionMovearray);
+              
+            $TopicTextarray = Array('show sliderleft','show sliderup','show sliderright','show rotateright','show rotateleft','show zoomin');
+             $TopicText = array_rand($TopicTextarray);
+             
+        //List of Key words
+        $keywords = array('1'=>"do",'2'=>"don't",'3'=>"remains",'4'=>"left",'5'=>"many","6"=>"lost",
+                              '7'=>"should",'8'=>"could",'9'=>"not",'10'=>"good",'11'=>"bad","12"=>"isn't",
+                              '13'=>"few",'14'=>"enough",'15'=>"shouldn't",'16'=>"couldn'17",'18'=>"less","19"=>"minus",
+                              '20'=>"add",'21'=>"is not",'22'=>"diff",'23'=>"sum",'25'=>"less","26"=>"exactly",
+                              '27'=>"complete",'28'=>"sometime",'29'=>"just",'30'=>"same",'31'=>"some", '32'=>"law",
+                              '33'=>"common",'34'=>"old" ,'35'=>"sub",'36'=>"order");
         
+
+ $newtopic="";
+ $newsection="";
+        for ($i=0;$i<count($realtopics);$i++)
+        {
+        $SizeRand = rand(20,60);
         ?>
+			
+		
+			<div class="step" 
+                data-x="<?php echo $x;?>" data-y="<?php echo $y; ?>" 
+                data-rotate="<?php echo $r; ?>"  data-scale="<?php echo $s; ?>" >
+             
+
+    			 <div style="float:Left; padding-right:100px;" >
+                	 <h3>Terms Of Service:</h3>
+					 <?php 
+						 for ($c=0;$c<count($realtopicsN);$c++)
+						 {
+
+					   ?> <h4> <?php 
+
+					   if ($realtopicsN[$c]==$realtopics[$i])
+					   {
+					  ?> <b class=" show zoomin" style="color:darkgreen"> <?php echo $c+1; echo " - "; echo ucwords($realtopicsN[$c]); ?> </b><?php
+					   }
+					   else {
+					  echo $c+1; echo " - "; echo ucwords($realtopicsN[$c]);
+					   }
+
+
+					   ?> </h4> <?php
+						 }
+						?>
+                    
+         </div>
+              
+                
+              <b  class="<?php echo $TopicTextarray[$TopicText]?>" style="<?php echo $array[$ColoRrand];?> ; font-size:<?php echo $SizeRand;?>% ;
+              <?php echo $Farray[$FontRrand];?>">
+              
+         <?php 
+          $realtopicsU= ucwords($realtopics[$i]);
+        foreach($keywords as $wordstatus => $word){
+              $word= ucwords($word);
+
+             if(preg_match_all('/'.$word.'/',$realtopicsU))
+             {
+                 
+	        $realtopicsU = str_replace($word,"<b style='font-family:Courier New ;color:red'>$word</b>", $realtopicsU);
+	        $newtopic = $realtopicsU;
+  
+             } 
+	else 
+	       {	       $newtopic = $realtopicsU;	       }
+	       }
+
+    echo  $newtopic;
+      
+          $ColoRrand = array_rand($array);   
+          $FontRrand = array_rand($Farray);
+          $TopicText = array_rand($TopicTextarray);
+         ?>
+         </b>
+                <p class="<?php echo $SectionMovearray[$SectionMove]?>" style="<?php echo $array[$ColoRrand];?>;  font-size:<?php echo $SizeRand;?>% ; <?php echo $Farray[$FontRrand];?>" >
+            <?php
+            $realsectionMix= ucwords($realsections[$i]);
+            
+             foreach($keywords as $wordstatus => $word){
+              $word= ucwords($word);
+               
+             if(preg_match_all('/'.$word.'/',$realsectionMix))
+             {
+               $realsectionMix = str_replace($word,"<b style='font-family:Courier New ;color:red'>$word</b>", $realsectionMix);
+	       $newsection = $realsectionMix;
+	        } 
+	else 
+	       { $newsection = $realsectionMix;}
+	       }
+             
+              echo  $newsection;   
+                ?>
+				
+                </p>
+               
+        </div>
         
         
         
-        <div class="step" data-x="<?php echo $x; $x+=1000; ?>" data-y="-1500">
+        <div class="step" data-x="<?php echo $x; $x+=1000; ?>" data-y="1500">
         <b><?php echo $realtopics[$i] ?></b><p> <?php echo $realsections[$i] ?></p>
         </div>
         
         
         <?php
+			$ColoRrand = array_rand($array);
+         $SectionMove = array_rand($SectionMovearray);
+        $x = $x+ rand(200,1000);
+        $y = $y+ rand(150,1500);
+        $r = rand(1,180);
+        $s = rand(1,2);		
+					
+					
         }
         
         
         ?>
-        
-        
-        
-        
-        
-        
+
         <div id="overview" class="step" data-x="3000" data-y="1500" data-scale="10">
         </div>
         </div>
@@ -171,102 +283,51 @@ $_SESSION["color"] = 'green';
         <p>Use a spacebar or arrow keys to navigate</p>
         </div>
     </div>
-    
-    <div class="container">
-    
-        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="headingOne">
-                  <h2 class="panel-title">
-                    <!--<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Demo</a>-->
-                   <span class='glyphicon glyphicon-tasks' aria-hidden='true'></span> Source Code
-                    
-                  </h2>
-                </div>
-                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-                    <div class="panel-body">
-                        
-                    </div>
-                </div>
-            </div>
-            <!--
-                <div class="panel panel-default">
-                    <div class="panel-heading" role="tab" id="headingTwo">
-                      <h4 class="panel-title">
-                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                         Use the App to generate presentation
-                        </a>
-                      </h4>
-                    </div>
-                    <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
-                        <div class="panel-body">
-                       
-                            
-                        </div>
-                    </div>
-                </div> 
-            -->
-        </div> 
-    
-    </div>
 
     <script>
     if ("ontouchstart" in document.documentElement) {
     document.querySelector(".hint").innerHTML = "<p>Tap on the left or right to navigate</p>";
     }
     </script>
+	
     
     <script src="js/impress.js"></script>
     <script>impress().init();</script>
-    <?php 
-        
-        
-        if($_POST["submit"]=="Email"){
-             if(isset($css)){
-              echo "<style>".$css."</style>";
-          }
-            //Using PHPMailer 
-             require 'PHPMailer/PHPMailerAutoload.php';
-             $mail = new PHPMailer;
-            
-            $mail->isSMTP();
-             $mail->Host = 'smtp.gmail.com'; 
-            $mail->SMTPAuth = plain;                            
-            $mail->Username = 'zameermfm@gmail.com';              
-            $mail->Password = 'jbadvgdwkyocqxja';                       
-            $mail->SMTPSecure = 'tls';                        
-            $mail->Port = 587;    
-            
-            //This is a free smtp, therefore, it will take some time to deliver the message.. it sucks! I know... Feel free to plug in your SMTP info.
-            
-            $email = $_POST['email'];
-            $text = $_POST["fulltext"];
-           
-            $mail->From = 'system@agreed.com';
-            $mail->FromName = 'system';
-            $mail->addAddress($email);     // Add a recipient
-            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-            $mail->isHTML(true);                                  // Set email format to HTML
-            $html = ob_get_contents();
-            ob_end_clean();
-            
-            $css = file_get_contents('./css/impress-demo.css', true);
-            $mail->Subject = 'Your document has arrived.';
-            $mail->Body = $html;
-          // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-            
-            if(!$mail->send()) {
-                echo 'Message could not be sent.<br>';
-                echo 'Mailer Error: ' . $mail->ErrorInfo;
-            } else {
-                echo 'Message has been sent';
-            }
-            
-          }
-    
-    
-    
-    ?>
-
+   
+	
+	<?php include("footer.html"); ?>
 </body>
 </html>
+
+<?php
+	$no = mt_rand(100,10000);
+
+	$page = ob_get_contents();
+
+
+	$from = strpos($page, '<body class="impress-not-supported">');
+	$to = strpos($page, '</body>');
+	$contents = substr($page, $from, $to - $from);
+
+	ob_end_clean();
+
+	$basename = base_url();
+
+	//file_put_contents('./view/'.$no.'document.html', $page);
+	$filename1 =  './view/'.$no.'create_own_int_tos.html';
+	$fullpage = './view/'.$no.'create_own_int_tos_fullpage.html';
+
+	$f = fopen($filename1, "w"); 
+	fwrite($f, $contents); 
+	fclose($f); 
+
+	$f2 = fopen($fullpage, "w"); 
+	fwrite($f2, $page); 
+	fclose($f2); 
+ 
+	$_SESSION['fullpagename'] = $fullpage;
+	$_SESSION['short'] = $filename1;
+	header("Location: presentation.php");
+	exit();
+
+?>
